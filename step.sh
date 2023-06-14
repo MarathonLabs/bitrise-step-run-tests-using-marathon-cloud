0844f1bd9bd810cf9aa58aad49df8cbd71d55779
Anton Malinskiy <anton@malinskiy.com>
2023-06-14 17:31:47 +1000

    feat(step): add real apks to test

--- step.sh
+++ step.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -ex
 
-marathon-cloud -api-key "$API_KEY" -apk "$BITRISE_APK_PATH" -testapk "$BITRISE_TEST_APK_PATH"
+marathon-cloud -api-key "$api_key" -apk "$BITRISE_APK_PATH" -testapk "$BITRISE_TEST_APK_PATH"
