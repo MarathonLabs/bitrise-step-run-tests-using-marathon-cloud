c9dc2b16ece5816c457e1493c45475de9449d603
Anton Malinskiy <anton@malinskiy.com>
2023-06-14 19:04:24 +1000

    feat(linux): use docker on linux since brew is missing

--- step.sh
+++ step.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 set -ex
 
-marathon-cloud -api-key "$api_key" -apk "$BITRISE_APK_PATH" -testapk "$BITRISE_TEST_APK_PATH"
+if [[ $OSTYPE == 'darwin'* ]]; then
+  echo 'macOS agents preinstall marathon-cloud cli via brew'
+  marathon-cloud -api-key "$api_key" -apk "$BITRISE_APK_PATH" -testapk "$BITRISE_TEST_APK_PATH"
+elif [[ $OSTYPE == 'linux'* ]]; then
+  echo 'using docker image $docker_image to pull the latest marathon-cloud cli version'
+  docker pull $docker_image
+  docker run -v "$BITRISE_APK_PATH":/work/app.apk -v "$BITRISE_TEST_APK_PATH":/work/appTest.apk -it --rm $docker_image -api-key "$api_key" -apk ./app.apk -testapk ./appTest.apk 
+fi
+
