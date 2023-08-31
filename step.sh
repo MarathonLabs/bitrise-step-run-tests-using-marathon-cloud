ddbcf9063a07342938fb626f86a659b15105404d
Evgenii Matsiuk (Eugene Matsyuk) <eugene.matsyuk@gmail.com>
2023-08-31 21:17:04 +0300

    feat(os-version-support): os_version parameter is supported; run_name and link are included too (#2)

--- step.sh
+++ step.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
 set -ex
 
+command_args=" -api-key $API_KEY -app $application -testapp $test_application -platform $platform"
+
+if [ -n "$os_version" ]; then
+    command_args="$command_args -os-version $os_version"
+fi
+
+if [ -n "$link" ]; then
+    command_args="$command_args -link $link"
+fi
+
+if [ -n "$run_name" ]; then
+    command_args="$command_args -name $run_name"
+fi
+
 if [[ $OSTYPE == 'darwin'* ]]; then
   echo 'macOS agents preinstall marathon-cloud cli via brew'
-  marathon-cloud -api-key "$api_key" -app "$application" -testapp "$test_application" -platform "$platform"
+  marathon-cloud $command_args
 elif [[ $OSTYPE == 'linux'* ]]; then
   echo 'using docker image $docker_image to pull the latest marathon-cloud cli version'
   docker pull $docker_image
-  docker run -v "$(pwd)":/work --rm $docker_image -api-key "$api_key" -app "$application" -testapp "$test_application" -platform "$platform"
+  docker run -v "$(pwd)":/work --rm $docker_image $command_args
 fi
-
