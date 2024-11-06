fa9c6f4ecedbf2babba03c3843da2195a399e6c4
Evgenii Matsiuk <eugene.matsyuk@gmail.com>
2024-11-06 20:43:56 +0300

    feat: support cli 1.0.35 (#5)

--- step.sh
+++ step.sh
@@ -6,7 +6,6 @@ command_args="run $run_platform"
 command_args="$command_args --api-key $api_key"
 command_args="$command_args --application $application"
 command_args="$command_args --test-application $test_application"
-command_args="$command_args --system-image $system_image"
 command_args="$command_args --isolated $isolated"
 
 if [ -n "$os_version" ]; then
@@ -23,6 +22,54 @@ fi
 
 if [ -n "$ignore_test_failures" ]; then
 	command_args="$command_args --ignore-test-failures $ignore_test_failures"
+fi
+
+if [ -n "$system_image" ]; then
+  if [ "$run_platform" = "android" ]; then
+    command_args="$command_args --system-image $system_image"
+  else
+    echo "Error: The --system-image parameter is not supported for iOS."
+    exit 1
+  fi
+fi
+
+if [ -n "$output" ]; then
+	command_args="$command_args --output $output"
+fi
+
+if [ -n "$filter_file" ]; then
+	command_args="$command_args --filter-file $filter_file"
+fi
+
+if [ -n "$device" ]; then
+	command_args="$command_args --device $device"
+fi
+
+if [ -n "$xcode_version" ]; then
+  if [ "$run_platform" = "ios" ]; then
+    command_args="$command_args --xcode-version $xcode_version"
+  else
+    echo "Error: The --xcode-version parameter is not supported for Android."
+    exit 1
+  fi
+fi
+
+if [ -n "$xctestplan_filter_file" ]; then
+  if [ "$run_platform" = "ios" ]; then
+    command_args="$command_args --xctestplan-filter-file $xctestplan_filter_file"
+  else
+    echo "Error: The --xctestplan-filter-file parameter is not supported for Android."
+    exit 1
+  fi
+fi
+
+if [ -n "$xctestplan_target_name" ]; then
+  if [ "$run_platform" = "ios" ]; then
+    command_args="$command_args --xctestplan-target-name $xctestplan_target_name"
+  else
+    echo "Error: The --xctestplan-target-name parameter is not supported for Android."
+    exit 1
+  fi
 fi
 
 if [[ $OSTYPE == 'darwin'* ]]; then
